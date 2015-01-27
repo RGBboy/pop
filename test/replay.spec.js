d75cb0f5c4f9d5ae6febde64d0f7cfcc8f2f921f
RGBboy <l-_-l@rgbboy.com>
2015-01-27 13:32:52 +1300

    Added buttons to replay component;

--- test/replay.spec.js
+++ test/replay.spec.js
@@ -11,7 +11,9 @@ var test = require('tape'),
     replay = require('../lib/replay'),
     createElement = require('virtual-dom/create-element'),
     elementClass = require('element-class'),
-    document = global.document;
+    event = require('synthetic-dom-events'),
+    document = global.document,
+    State = require('./helpers/state');
 
 /**
 * replay
@@ -23,15 +25,77 @@ test('replay should be a function', function (t) {
 });
 
 test('replay should return a replay component', function (t) {
-  var element;
+  var element,
+      state,
+      replayButton,
+      menuButton;
 
-  t.plan(1);
+  t.plan(9);
 
-  element = createElement(replay());
+  state = State();
+  element = createElement(replay(state));
   document.body.appendChild(element);
 
   t.ok(elementClass(element).has('Replay'), 'Component has class: Replay');
 
+  replayButton = element.querySelector('.Replay-replay');
+  t.ok(replayButton, 'Component has button');
+  t.equal(replayButton.innerText, 'Replay');
+  t.ok(elementClass(replayButton).has('Button'), 'Button has class: Button');
+  t.ok(elementClass(replayButton).has('Button--default'), 'Button has class: Button--default');
+
+  menuButton = element.querySelector('.Replay-menu');
+  t.ok(menuButton, 'Component has button');
+  t.equal(menuButton.innerText, 'Menu');
+  t.ok(elementClass(menuButton).has('Button'), 'Button has class: Button');
+  t.ok(elementClass(menuButton).has('Button--default'), 'Button has class: Button--default');
+
   document.body.removeChild(element);
+
+});
+
+test('when replay button is clicked it should call state.updateView with countdown', function (t) {
+  var element,
+      state,
+      button,
+      updateView;
+
+  t.plan(1);
+
+  updateView = function (view, e) {
+    t.equal(view, 'countdown');
+    document.body.removeChild(element);
+  };
+
+  state = State({ updateView: updateView });
+  element = createElement(replay(state));
+  document.body.appendChild(element);
+
+  button = element.querySelector('.Replay-replay');
+
+  button.dispatchEvent(event('click'));
+
+});
+
+test('when menu button is clicked it should call state.updateView with title', function (t) {
+  var element,
+      state,
+      button,
+      updateView;
+
+  t.plan(1);
+
+  updateView = function (view, e) {
+    t.equal(view, 'title');
+    document.body.removeChild(element);
+  };
+
+  state = State({ updateView: updateView });
+  element = createElement(replay(state));
+  document.body.appendChild(element);
+
+  button = element.querySelector('.Replay-menu');
+
+  button.dispatchEvent(event('click'));
 
 });
