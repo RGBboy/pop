febaba8b55510719786026beee04c41025b6c002
RGBboy <l-_-l@rgbboy.com>
2017-08-27 18:32:21 +0100

    refactor;

--- src/Main.elm
+++ src/Main.elm
@@ -4,7 +4,6 @@ import AnimationFrame
 import Html as H exposing (Html)
 import Html.Attributes as A
 import Html.Events as E
-import Html.Keyed as K
 import Task exposing (Task)
 import Time exposing (Time)
 
@@ -12,28 +11,23 @@ import Time exposing (Time)
 
 -- MODEL
 
-type View
+type Model
   = Loading
   | Title
   | Countdown Time
   | Play Time
   | Replay
 
-type alias Model =
-  { view : View
-  }
-
 init : (Model, Cmd Msg)
 init =
-  ( { view = Loading
-    }
-  , Task.perform identity (Task.succeed (UpdateView Title))
+  ( Loading
+  , Task.perform identity (Task.succeed (UpdateState Title))
   )
 
-initCountdown : View
+initCountdown : Model
 initCountdown = Countdown (3 * Time.second)
 
-initPlay : View
+initPlay : Model
 initPlay = Play (3 * Time.second)
 
 
@@ -41,18 +35,18 @@ initPlay = Play (3 * Time.second)
 -- UPDATE
 
 type Msg
-  = UpdateView View
+  = UpdateState Model
   | Tick Time
 
 update : Msg -> Model -> (Model, Cmd msg)
 update message model =
   case message of
-    UpdateView view ->
-      ( { model | view = view }
+    UpdateState model ->
+      ( model
       , Cmd.none
       )
     Tick delta ->
-      case model.view of
+      case model of
         Countdown timeLeft ->
           let
             time = timeLeft - delta
@@ -62,7 +56,7 @@ update message model =
               else
                 initPlay
           in
-            ( { model | view = view}
+            ( view
             , Cmd.none
             )
         Play timeLeft ->
@@ -74,7 +68,7 @@ update message model =
               else
                 Replay
           in
-            ( { model | view = view}
+            ( view
             , Cmd.none
             )
         _ -> (model, Cmd.none)
@@ -91,27 +85,12 @@ subscriptions model =
 
 -- VIEW
 
-viewToString : View -> String
-viewToString view =
-  case view of
-    Loading -> "Loading"
-    Title -> "Title"
-    Countdown _ -> "Countdown"
-    Play _ -> "Play"
-    Replay -> "Replay"
-
-app : View -> List (Html msg) -> Html msg
-app view children =
+app : Html msg -> Html msg
+app children =
   H.div [ A.id "App", A.class "App u-posAbsoluteCenter"]
-    [ H.div [] -- transition group App-animation
-        [ K.node "div" []
-            [ ( viewToString view,
-                H.div [ A.class "App-screen Arrange Arrange--middle"]
-                  [ H.div [ A.class "Arrange-sizeFill"] children
-                  ]
-              )
-            ]
-        ]
+    [ H.div [ A.class "App-screen Arrange Arrange--middle"]
+      [ H.div [ A.class "Arrange-sizeFill"] [ children ]
+      ]
     ]
 
 button : msg -> String -> Html msg
@@ -133,7 +112,7 @@ title : Html Msg
 title =
   H.div [ A.class "Title" ]
     [ H.h1 [ A.class "u-textCenter" ] [ H.text "Pop!" ]
-    , buttonGroup [ button (UpdateView initCountdown) "Play" ]
+    , buttonGroup [ button (UpdateState initCountdown) "Play" ]
     ]
 
 countdown : Time -> Html msg
@@ -162,23 +141,23 @@ replay score =
     [ H.h2 [ A.class "u-textCenter" ] [ H.text "Time's Up!"]
     , H.p [ A.class "u-textCenter" ] [ H.text ("You scored " ++ (toString score) ++ " points!") ]
     , buttonGroup
-        [ button (UpdateView initCountdown) "Replay"
-        , button (UpdateView Title) "Menu"
+        [ button (UpdateState initCountdown) "Replay"
+        , button (UpdateState Title) "Menu"
         ]
     ]
 
-viewToHtml : View -> Html Msg
-viewToHtml view =
-  case view of
-    Loading -> loading
-    Title -> title
-    Countdown timeLeft -> countdown timeLeft
-    Play timeLeft -> play timeLeft 5
-    Replay -> replay 5
-
 view : Model -> Html Msg
 view model =
-  app model.view [(viewToHtml model.view)]
+  let
+    content =
+      case model of
+        Loading -> loading
+        Title -> title
+        Countdown timeLeft -> countdown timeLeft
+        Play timeLeft -> play timeLeft 5
+        Replay -> replay 5
+  in
+    app content
 
 
 
